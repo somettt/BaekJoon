0c6117843ac721d1d501e65128d0a0555d31cbba
somettt <somethinaws@gmail.com>
2025-12-29 18:08:14 +0900

    C

---
+++ Java/5622_Dial.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if (input.trim().isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        String[] word = input.trim().split("\\s+");
+
+        System.out.println(word.length);
+    }
+}
