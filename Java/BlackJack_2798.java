ee7dc7fee758956691598e45486dedc7109c15e6
somettt <somethinaws@gmail.com>
2026-01-10 19:11:49 +0900

    26/1/9

--- Java/BlackJack_2798.java
+++ Java/BlackJack_2798.java
@@ -4,7 +4,6 @@ public class BlackJack_2798 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("입력");
 
         int n = sc.nextInt(); // 주어지는 카드의 수
         int m = sc.nextInt(); // 21
@@ -14,11 +13,9 @@ public class BlackJack_2798 {
             cards[i] = sc.nextInt();
         }
         int biggest=0;
-        System.out.println("입력완료");
 
         for(int i=0; i<n; i++){
             int x = cards[i];
-            System.out.println("x:"+x);
             for (int j=i+1; j<n; j++){
                 int y = cards[j];
                 for(int k=j+1; k<n; k++){
---
+++ Java/FindingNumbers_1920.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class FindingNumbers_1920 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        TreeSet<Integer> tree = new TreeSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            tree.add(sc.nextInt()); //n은 트리에 저장
+        }
+
+        int m = sc.nextInt();
+        int[] arr = new int[m]; //m 저장용 배열
+
+        for(int i=0; i<m; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<m; i++){
+            int a = arr[i];
+            if (tree.contains(a)) sb.append(1).append('\n');
+            else sb.append(0).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+}
